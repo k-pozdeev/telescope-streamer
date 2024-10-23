e026d389c4be70743179f471167048c64c796844
keer <mayor.stopko@gmail.com>
2024-10-23 18:01:55 +0300

    smbus 1

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import smbus
 import time
 
 # for RPI version 1, use "bus = smbus.SMBus(0)"
-bus = smbus.SMBus(0)
+bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
 address = 0x40
